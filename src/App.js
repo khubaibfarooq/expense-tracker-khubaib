73123c374c132e84b99acd89030d12ee5c0e4a27
khubaib <khubaibfarooq0@gmail.com>
2021-03-12 23:37:24 +0500

    header,balance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import {Header} from './components/Header';
+import {Balance} from './components/Balance';
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Balance/>
     </div>
   );
 }
